110eeb9754825f62ff207fa4cadf53dfdb6d8e81
dimsssss <caruen@hanmail.net>
2022-05-11 08:57:18 +0900

    feat: user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('user', {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: 'ENUM',
+            allowNull: false,
+            defaultValue: 'EMPLOYEE'
+        },
+        createdAt: {
+            type: "TIMESTAMP",
+            allowNull: true,
+        },
+        updatedAt: {
+            type: "TIMESTAMP",
+            allowNull: true
+        },
+    }, {
+    });
+
+    return user;
+}
